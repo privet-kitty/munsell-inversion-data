4c5362be9d753c3d3d8d2e7f5440390cc33bbffd
privet-kitty <privet.kitty99@gmail.com>
2018-01-18 19:00:52 +0100

    add information about RGB to Munsell

--- gen-mid.lisp
+++ gen-mid.lisp
@@ -1,3 +1,7 @@
+;;
+;; This is a script file to generate Munsell inversion datas.
+;;
+
 (in-package :cl-user)
 
 (require :dufy)
@@ -6,10 +10,10 @@
 (defparameter current-path *load-pathname*)
 
 (defparameter task-lst
-  (list (cons "srgb-d65" dufy:srgb)
-	(cons "srgb-d50" dufy:srgbd50)
-	(cons "adobergb-d65" dufy:adobe)
-	(cons "adobergb-d50" dufy:adobed50)))
+  (list (cons "mid-srgb-d65" dufy:srgb)
+	(cons "mid-srgb-d50" dufy:srgbd50)
+	(cons "mid-adobergb-d65" dufy:adobe)
+	(cons "mid-adobergb-d50" dufy:adobed50)))
 
 (defun do-task (basename rgbspace)
   (let* ((filename (concatenate 'string basename ".dat"))
